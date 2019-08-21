b7d77dfba1a41152f5815ae9291e68657bbc40b8
Johan <johanhe64@gmail.com>
2019-08-21 16:55:31 -0300

    cambios
    dede

--- index.php
+++ index.php
@@ -68,8 +68,8 @@
 
     <!-- Page Content -->
     <div id="page-content-wrapper">
-
-      <nav class="navbar navbar-expand-lg navbar-dark main-color border-bottom">
+      
+ <nav class="navbar navbar-expand-lg navbar-dark main-color border-bottom">
         <button class="btn btn-primary" id="menu-toggle" ><i class="fas fa-bars"></i></button>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -77,23 +77,15 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-
-          <ul class="navbar-nav  ml-auto mt-2">
-            <li class="nav-item">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+            <li class="nav-item mr-4">
               <a class="nav-link" href="#">Ayuda</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item mr-4">
               <a class="nav-link" href="#">Servicios</a>
             </li>
-
             <li class="nav-item active">
-              
-              <a class="nav-link" data-toggle="modal" data-target="#login-modal">
-                Iniciar sesion <i class="fas fa-user-circle">
-                </i>
-              </a>
-
+              <a class="nav-link" href="#">iniciar sesion<i class="fas fa-user-circle"></i></a>
             </li>
           </ul>
         </div>
--- vista-servicios.php
+++ vista-servicios.php
@@ -56,7 +56,7 @@
     <!-- Page Content -->
     <div id="page-content-wrapper">
 
-     <nav class="navbar navbar-expand-lg navbar-dark main-color border-bottom">
+      <nav class="navbar navbar-expand-lg navbar-dark main-color border-bottom">
         <button class="btn btn-primary" id="menu-toggle" ><i class="fas fa-bars"></i></button>
 
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -68,11 +68,11 @@
             <li class="nav-item mr-4">
               <a class="nav-link" href="#">Ayuda</a>
             </li>
-            <li class="nav-item">
+            <li class="nav-item mr-4">
               <a class="nav-link" href="#">Servicios</a>
             </li>
             <li class="nav-item active">
-              <a class="nav-link" href="#">Cerrar sesion<i class="fas fa-user-circle"></i></a>
+              <a class="nav-link" href="#">iniciar sesion<i class="fas fa-user-circle"></i></a>
             </li>
           </ul>
         </div>
