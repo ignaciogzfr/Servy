37cd7afb348a7f841cf181df23f8cd8ab61623de
Ignacio Gonzalez <ignaciogzfr@users.noreply.github.com>
2019-08-22 16:01:11 -0400

    orden? no se que hace esto

--- componentes/sidenav.php
+++ componentes/sidenav.php
@@ -1,7 +1,4 @@
-  <div class="d-flex" id="wrapper" tabindex="-1">
-
-    <!-- Sidebar -->
-    <div class="mdb-color  text-white border-right" id="sidebar-wrapper">
+<div class="mdb-color  text-white border-right" id="sidebar-wrapper">
 
 
       <div class="sidebar-heading mdb-color">Menu</div>
@@ -21,4 +18,5 @@
       </div>
 
 
-    </div>
+</div>
+<!-- /#sidebar-wrapper -->
