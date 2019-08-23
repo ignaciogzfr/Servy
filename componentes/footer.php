26071420e9fb9d4287bef8918c1331422a8cf64d
Ignacio González F <ignacio.gzfr@gmail.com>
2019-08-22 20:41:21 -0400

    123

---
+++ componentes/footer.php
@@ -0,0 +1,20 @@
+<!-- Footer -->
+<footer class="page-footer mdb-color font-small lighten-1 text-white">
+
+  <!-- Copyright -->
+  <div class="row text-center">
+    
+  <div class="col col-md-4 my-2"><img src="img/placeholder.png" height="100" width="100"></div>
+  <div class="col col-md-4 mt-2">OJO, Servy provee un servicio de atencion, la aplicacion no se hace responsable si los tecnicos no cumplen satisfactoriamente con el servicio requerido.</div>
+  <div class="col col-md-4 mt-2">
+    <div class="row">
+    <a class="col col-md-12 my-1"href="#">Link1</a>
+    <a class="col col-md-12 my-1"href="#">Link2</a>
+    <a class="col col-md-12 my-1"href="#">Link3</a>
+    </div>
+  </div>
+
+  </div>
+  <!-- Copyright -->
+
+</footer>
