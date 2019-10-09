e6fb5b0237309aadbb0a8260f97e17b9f0b33a83
Ignacio Gonzalez <ignaciogzfr@users.noreply.github.com>
2019-10-09 18:36:40 -0300

    a

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -2,7 +2,7 @@
 
 class Conexion{
 	static public function conectar(){
-		$link = new PDO("remotemysql.com:3306;dbname=hd8LHE0bKS",'hd8LHE0bKS','mzCSJRrPI',
+		$link = new PDO("remotemysql.com:3306;dbname=hd8LHE0bKS",'hd8LHE0bKS','J1kFgHhxkx',
 			array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8")
 		);
 
