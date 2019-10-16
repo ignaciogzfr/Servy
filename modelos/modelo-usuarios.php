dbf54990fe4c807eff8c852b05e55097e2a3e5b6
Johan <johanhe64@gmail.com>
2019-10-16 17:47:29 -0300

    interfaz moderacion (faltan las acciones)

--- modelos/modelo-usuarios.php
+++ modelos/modelo-usuarios.php
@@ -5,11 +5,11 @@ require_once("conexion.php");
 Class Usuarios{
 
 	static public function getUsuarios(){
-
+				//obtiene todos los datos de la  tabla usuario
 		$con = Conexion::conectar();
 		$sql = $con->prepare("SELECT * FROM usuario");
 		$sql->execute();
-		return $sql->fecthAll(PDO::FECTH_ASSOC);
+		return $sql->fetchAll(PDO::FETCH_ASSOC); 
 
 	}
 
