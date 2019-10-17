d86e1e917f765fe6f75866d23a5f774d13e2c5e8
Johan <johanhe64@gmail.com>
2019-10-17 20:33:47 -0300

    el otro

--- modelos/modelo-publicaciones.php
+++ modelos/modelo-publicaciones.php
@@ -7,7 +7,7 @@ Class Publicaciones{
 	static public function getPublicaciones(){
 	 //Obtiene todo los datos  de la tabla publicacion, ademas de nombre_usuario y el tipo_servicio de sus respectivas tablas
 		$con = Conexion::conectar();
-		$sql = $con->prepare("SELECT p.*, u.nombre_usuario, t.tipo_servicio  FROM publicacion p, usuario u, tipo_servicio t WHERE p.id_usuario = u.id_usuario and p.id_tipo_servicio = t.id_tipo_servicio");
+		$sql = $con->prepare("SELECT p.*, u.nombre_usuario, t.tipo_servicio  FROM publicacion p, usuario u, tipo_servicio t WHERE p.id_usuario = u.id_usuario and p.id_tipo_servicio = t.id_tipo_servicio and estado_publicacion = 'Pendiente'");
 		$sql->execute();
 		return $sql->fetchAll(PDO::FETCH_ASSOC); 
 	}
